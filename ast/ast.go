22f7d7cb05885680b8d589cb4a3990d29bc33669
Ivor Popovic <ivorpopovic@IMP.local>
2017-12-16 18:19:57 +1100

    Added support for parsing let and return statements - still can't parse expressions in them though.

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,17 @@ package ast
 
 import "monkey/token"
 
+type ReturnStatement struct {
+	Token token.Token // the token.RETURN token
+	Value Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier
--- token/token.go
+++ token/token.go
@@ -3,58 +3,58 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
 const (
-	ILLEGAL 	= "ILLEGAL"
-	EOF 		= "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
 	// Identifiers and literals
-	IDENT 		= "IDENT" // things like: add, foobar, x, y
-	INT 		= "INT" // 123, 123151, -231
+	IDENT = "IDENT" // things like: add, foobar, x, y
+	INT   = "INT"   // 123, 123151, -231
 
 	// Operators
-	ASSIGN 		= "="
-	PLUS 		= "+"
-	MINUS 		= "-"
-	NOT 		= "!"
-	MULT 		= "*"
-	DIVIDE 		= "/"
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	NOT    = "!"
+	MULT   = "*"
+	DIVIDE = "/"
 
-	LT 			= "<"
-	GT 			= ">"
-	EQ 			= "=="
-	NEQ 		= "!="
+	LT  = "<"
+	GT  = ">"
+	EQ  = "=="
+	NEQ = "!="
 
 	// Delimiters
-	COMMA 		= ","
-	SEMICOLON 	= ";"
+	COMMA     = ","
+	SEMICOLON = ";"
 
-	LPAREN 		= "("
-	RPAREN 		= ")"
-	LBRACE 		= "{"
-	RBRACE 		= "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
 	// Keywords
-	FUNCTION 	= "FUNCTION"
-	LET 		= "LET"
-	IF			= "IF"
-	ELSE		= "ELSE"
-	RETURN		= "RETURN"
-	TRUE		= "TRUE"
-	FALSE		= "FALSE"
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
 )
 
-var keywords = map[string]TokenType {
-	"fn": FUNCTION,
-	"let": LET,
-	"if": IF,
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
 	"return": RETURN,
-	"else": ELSE,
-	"true": TRUE,
-	"false": FALSE,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
